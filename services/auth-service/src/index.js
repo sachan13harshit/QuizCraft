6553614a514396f39388eebe5ddb480fb2496d12
harshitssachan <harshit.sachan@scaler.com>
2025-06-10 11:35:57 +0530

    Fixed

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -34,6 +34,14 @@ app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Sever is running',
+  });
+});
+
 // Health check endpoint
 app.get('/health', (req, res) => {
   res.status(200).json({
